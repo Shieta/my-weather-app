7ae960ac07a69e72e075c1d9d7023e84f5a02373
Shieta <98153977+Shieta@users.noreply.github.com>
2022-04-25 21:12:50 +1000

    add apiKey and apiUrl
    add axios link url and start develop js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data.main);
+}
 
+let apiKey = "a22b96dbbce104ed6797b40b52579d0c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
