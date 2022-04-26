968a83ef0268cc463d77098b0a117e7b45fb025a
Shieta <98153977+Shieta@users.noreply.github.com>
2022-04-26 19:14:41 +1000

    add JS of displayTemperature

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 function displayTemperature(response) {
-  console.log(response.data.main);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#temp-describe");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "a22b96dbbce104ed6797b40b52579d0c";
